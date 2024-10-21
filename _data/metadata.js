d06d9c2f30193ae7376ea61ce310bc14bbc0a252
Bhareth Kachroo <bhareth.kachroo@gmail.com>
2024-10-20 20:33:35 -0400

    Edit metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Bhareth Kachroo Personal Website",
+	url: "https://bkachroo.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I'm an engineer",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Bhareth Kachroo",
+		email: "bhareth.kachroo@gmail.com",
+		url: "https://bkachroo.com"
 	}
 }
